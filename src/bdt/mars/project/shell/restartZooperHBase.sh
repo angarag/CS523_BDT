dcd7688c438932ccbc56c3355a2f7460ead57df6
Angarag <mars0411@gmail.com>
2019-05-18 21:13:16 -0700

    done

--- src/bdt/mars/project/shell/restartZooperHBase.sh
+++ src/bdt/mars/project/shell/restartZooperHBase.sh
@@ -1,11 +1,12 @@
+sudo service zookeeper-server stop
+sudo service zookeeper-server start
 sudo service hbase-regionserver stop
+sudo service hbase-regionserver start
 sudo service hbase-master stop
+sudo service hbase-master start
+//hbase shell
 //sudo fuser -k -n tcp 2181
 //sudo fuser -k -n tcp 9090
 //sudo fuser -k -n tcp 9092
-sudo service hbase-regionserver start
-sudo service hbase-master start
-//hbase shell
-sudo service zookeeper-server start
-sudo service zookeeper-server stop
+
 
--- src/bdt/mars/project/v1/HiveUtil.java
+++ src/bdt/mars/project/v1/HiveUtil.java
@@ -15,13 +15,14 @@ public class HiveUtil {
 				.appName("Java Spark Hive Example")
 				.config("spark.master", "local").enableHiveSupport()
 				.getOrCreate();
-
+		/*
 		spark.sql("CREATE TABLE IF NOT EXISTS vote (voteFor STRING, user STRING, date TIMESTAMP, count INT) USING hive");
 		String s = "create external table election (id STRING, voteFor STRING, user STRING, count STRING, date TIMESTAMP) STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler' WITH SERDEPROPERTIES(\"hbase.columns.mapping\" = \":key,vote_details:voteFor,vote_details:user,vote_details:count,vote_details:timestamp\") TBLPROPERTIES(\"hbase.table.name\"=\"election\")";
 		spark.sql(s);
+		
 		spark.sql("LOAD DATA LOCAL INPATH 'input/election_votes.txt' OVERWRITE INTO TABLE vote");
-		// spark.sql("LOAD DATA INPATH 'user/cloudera/input/election_votes.txt' OVERWRITE INTO TABLE vote");
-		// spark.sql("SELECT candidate,COUNT(*) FROM vote GROUP BY candidate").show();
-
+		 spark.sql("LOAD DATA INPATH 'user/cloudera/input/election_votes.txt' OVERWRITE INTO TABLE vote");
+		*/
+		spark.sql("SELECT candidate,COUNT(*) FROM vote GROUP BY candidate").show();
 	}
 }
