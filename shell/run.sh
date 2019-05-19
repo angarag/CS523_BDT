3a2d1f1af6742cf619a872be45742c7b56ac9226
cloudera <cloudera@quickstart.cloudera>
2019-05-19 15:43:23 -0700

    duusgay

--- shell/run.sh
+++ shell/run.sh
@@ -2,6 +2,12 @@
 spark-submit --class bdt.mars.project.v1.Producer --master yarn CS523_986689.jar
 sleep 3s
 mkdir input
+mkdir conf
+touch conf/app.properties
+URL=$Elastic_URL
+PASSWORD=$Elastic_password
+echo "url=$URL" >> conf/app.properties
+echo "password=$PASSWORD" >> conf/app.properties
 spark-submit --class bdt.mars.project.v1.Consumer --master yarn CS523_986689.jar
-sleep 30s
+sleep 300s
 spark-submit --class bdt.mars.project.v1.HiveUtil --master yarn CS523_986689.jar
