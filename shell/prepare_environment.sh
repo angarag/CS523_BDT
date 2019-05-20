486a5df4fd55737164eacfa7e7a4de4030b29045
cloudera <cloudera@quickstart.cloudera>
2019-05-20 08:41:37 -0700

    shells improved

--- shell/prepare_environment.sh
+++ shell/prepare_environment.sh
@@ -2,3 +2,23 @@
 cd ~/Downloads/kafka_2.12-2.2.0
 bin/kafka-server-start.sh config/server.properties
 bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic election
+
+#Create election table in HBase
+hbase shell << create 'election', 'vote_details'
+
+#Create Hive-Hbase mapping & enable Hive temp directory
+sudo chmod 777 /tmp/hive
+hive << CREATE EXTERNAL TABLE election(id STRING, voteFor STRING, user STRING, count STRING, date TIMESTAMP) ROW FORMAT SERDE 'org.apache.hadoop.hive.hbase.HBaseSerDe' STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler' WITH SERDEPROPERTIES("hbase.columns.mapping" = ":key,vote_details:voteFor,vote_details:user,vote_details:count,vote_details:timestamp") TBLPROPERTIES("hbase.table.name"="election");
+
+#Create input directory for local log file
+mkdir input
+
+#Define properties for Elastic connection
+mkdir conf
+touch conf/app.properties
+URL=$Elastic_URL
+PASSWORD=$Elastic_password
+echo "url=$URL" >> conf/app.properties
+echo "password=$PASSWORD" >> conf/app.properties
+
+#Clear the table in Elastic search with elasticSchema.json
--- shell/run.sh
+++ shell/run.sh
@@ -1,24 +1,12 @@
-#Run Kafka
-cd ~/Downloads/kafka_2.12-2.2.0
-bin/kafka-server-start.sh config/server.properties
-bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic election
-#Create election table in HBase
-hbase shell << create 'election', 'vote_details'
-#Create Hive-Hbase mapping
-hive << CREATE EXTERNAL TABLE election(id STRING, voteFor STRING, user STRING, count STRING, date TIMESTAMP) ROW FORMAT SERDE 'org.apache.hadoop.hive.hbase.HBaseSerDe' STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler' WITH SERDEPROPERTIES("hbase.columns.mapping" = ":key,vote_details:voteFor,vote_details:user,vote_details:count,vote_details:timestamp") TBLPROPERTIES("hbase.table.name"="election");
-#Run Producer: After exporting it as a runnable jar, run the followings (it is assumed that the jar file is exported to Desktop folder):
+#Run Producer program
+#After exporting it as a runnable jar, run the followings (it is assumed that the jar file is exported to Desktop folder):
 cd ~/Desktop
 spark-submit --class bdt.mars.project.v1.Producer --master yarn CS523_986689.jar
 sleep 3s
-mkdir input
-#Define properties for Elastic connection
-mkdir conf
-touch conf/app.properties
-URL=$Elastic_URL
-PASSWORD=$Elastic_password
-echo "url=$URL" >> conf/app.properties
-echo "password=$PASSWORD" >> conf/app.properties
-#Run the Consumer program finally
+
+#Run Consumer program
 spark-submit --class bdt.mars.project.v1.Consumer --master yarn CS523_986689.jar
 sleep 300s
+
+#Run SparkSQL program
 spark-submit --class bdt.mars.project.v1.HiveUtil --master yarn CS523_986689.jar
