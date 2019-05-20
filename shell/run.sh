d96b8a1b114912ec2aa952e2c79eaaf309354ba1
cloudera <cloudera@quickstart.cloudera>
2019-05-20 08:22:50 -0700

    done

--- shell/run.sh
+++ shell/run.sh
@@ -6,17 +6,19 @@ bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-fac
 hbase shell << create 'election', 'vote_details'
 #Create Hive-Hbase mapping
 hive << CREATE EXTERNAL TABLE election(id STRING, voteFor STRING, user STRING, count STRING, date TIMESTAMP) ROW FORMAT SERDE 'org.apache.hadoop.hive.hbase.HBaseSerDe' STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler' WITH SERDEPROPERTIES("hbase.columns.mapping" = ":key,vote_details:voteFor,vote_details:user,vote_details:count,vote_details:timestamp") TBLPROPERTIES("hbase.table.name"="election");
-#After exporting it as a runnable jar, run the followings (it is assumed that the jar file is exported to Desktop folder):
+#Run Producer: After exporting it as a runnable jar, run the followings (it is assumed that the jar file is exported to Desktop folder):
 cd ~/Desktop
 spark-submit --class bdt.mars.project.v1.Producer --master yarn CS523_986689.jar
 sleep 3s
 mkdir input
+#Define properties for Elastic connection
 mkdir conf
 touch conf/app.properties
 URL=$Elastic_URL
 PASSWORD=$Elastic_password
 echo "url=$URL" >> conf/app.properties
 echo "password=$PASSWORD" >> conf/app.properties
+#Run the Consumer program finally
 spark-submit --class bdt.mars.project.v1.Consumer --master yarn CS523_986689.jar
 sleep 300s
 spark-submit --class bdt.mars.project.v1.HiveUtil --master yarn CS523_986689.jar
